8e800bb4c8a3fee69c416a8d3e08c7e3b186ee4b
AntonFartovii <anton@rza.by>
2024-02-05 15:17:08 +0300

    feat: create readline

---
+++ index.js
@@ -0,0 +1,25 @@
+import { createInterface } from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process';
+import process from 'node:process';
+
+const readline = createInterface({input, output});
+readline.on('line', async (data) => {
+    readline.pause();
+
+})
+
+readline.on('SIGINT', () => {
+    readline.close();
+})
+
+readline.on ('close',  () => {
+    process.exit(0);
+})
+
+readline.on ('error',  ()=> {
+});
+
+process.on('exit', () => {
+})
+
+
